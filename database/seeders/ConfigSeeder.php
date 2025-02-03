33727091758f49e493bbea17c63524d077e3b86a
Dwi Arfian <dwiarfiann@gmail.com>
2025-02-03 22:56:00 +0700

    Add new configuration options to ConfigSeeder

--- database/seeders/ConfigSeeder.php
+++ database/seeders/ConfigSeeder.php
@@ -20,9 +20,24 @@ class ConfigSeeder extends Seeder
                 'is_active' => 0
             ],
             [
+                'key' => 'yes_choice',
+                'value' => 'Phising',
+                'is_active' => 1
+            ],
+            [
+                'key' => 'no_choice',
+                'value' => 'Tidak',
+                'is_active' => 1
+            ],
+            [
                 'key' => 'qr_donasi',
                 'value' => '-',
-                'is_active' => 0
+                'is_active' => 1
+            ],
+            [
+                'key' => 'link_pengaduan',
+                'value' => 'https://forms.gle/qQ71ZdyXDoz34NF46',
+                'is_active' => 1
             ]
         ]);
     }
